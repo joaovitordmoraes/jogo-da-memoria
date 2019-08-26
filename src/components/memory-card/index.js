9a58dd28c553edff1327c400e86cece65ba9b193
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-08-26 19:44:52 +0000

    css no js do memorycard

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -8,4 +8,49 @@ const createMemoryCard = ({src, alt, estado}) => `
     </article>
 `;
 
+const $head = document.querySelector("head");
+const $style = document.createElement("style");
+
+$style.textContent = `
+    .memory_card {
+        background-color: #f25a70;
+        height: 155px;
+        width: 155px;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+        position: relative;
+        cursor: pointer;
+    }
+
+    .memory_card.-turn_up {
+        background-color: #ffffff;
+    }
+
+    .memory_card > .icon {
+        height: 100px;
+        width: 100px;
+        z-index: 2;
+    }
+
+    .memory_card.-turn_up > .icon {
+        margin-top: -10px;
+    }
+
+    .memory_card.-turn_up::before {
+        content: '';
+        position: absolute;
+        height: 94px;
+        width: 94px;
+        bottom: 22px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        z-index: 1;
+    }
+`
+
+$head.insertBefore($style, null);
+
 const handleClick = () => console.log("Clicado");
