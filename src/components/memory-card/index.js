d741ed1b81fa52a1d4e966a8d07d309cbaf8a0e6
João Vitor <joaovitordmoraes@gmail.com>
2019-08-26 23:45:14 -0300

    Aula 27

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,56 +1,64 @@
-const createMemoryCard = ({src, alt, estado}) => `
-    <article class="memory_card ${estado}" onClick="handleClick()">
-        <img 
-            src="${src}" 
-            alt="${alt}" 
-            class="icon"
-        />
-    </article>
-`;
+const memoryCard = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
 
-const $head = document.querySelector("head");
-const $style = document.createElement("style");
+    $style.textContent = `
+        .memory_card {
+            background-color: #f25a70;
+            height: 155px;
+            width: 155px;
+            border-radius: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+            position: relative;
+            cursor: pointer;
+        }
 
-$style.textContent = `
-    .memory_card {
-        background-color: #f25a70;
-        height: 155px;
-        width: 155px;
-        border-radius: 30px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-        position: relative;
-        cursor: pointer;
-    }
+        .memory_card.-turn_up {
+            background-color: #ffffff;
+        }
 
-    .memory_card.-turn_up {
-        background-color: #ffffff;
-    }
+        .memory_card > .icon {
+            height: 100px;
+            width: 100px;
+            z-index: 2;
+        }
 
-    .memory_card > .icon {
-        height: 100px;
-        width: 100px;
-        z-index: 2;
-    }
+        .memory_card.-turn_up > .icon {
+            margin-top: -10px;
+        }
 
-    .memory_card.-turn_up > .icon {
-        margin-top: -10px;
-    }
+        .memory_card.-turn_up::before {
+            content: '';
+            position: absolute;
+            height: 94px;
+            width: 94px;
+            bottom: 22px;
+            background-color: #d4d4d4;
+            border-radius: 50%;
+            z-index: 1;
+        }
+    `
 
-    .memory_card.-turn_up::before {
-        content: '';
-        position: absolute;
-        height: 94px;
-        width: 94px;
-        bottom: 22px;
-        background-color: #d4d4d4;
-        border-radius: 50%;
-        z-index: 1;
-    }
-`
+    $head.insertBefore($style, null);
 
-$head.insertBefore($style, null);
+    return ({src, alt, estado}) => `
+        <article class="memory_card ${estado}" onClick="handleClick()">
+            <img 
+                src="${src}" 
+                alt="${alt}" 
+                class="icon"
+            />
+        </article>
+    `;
+};
 
-const handleClick = () => console.log("Clicado");
+
+
+const handleClick = () => {
+    const $card = event.target;
+    $card.classList.remove('-turn_up');
+    
+};
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,5 +1,8 @@
 const $root = document.querySelector("#root");
+
 const $cardsWrapper = createCardsWrapper();
+const createMemoryCard = memoryCard();
+
 const $memoryCard = createMemoryCard({
     src: "images/icon-collabcode.svg", 
     alt: "Gueio - mascote da CollabCode"
