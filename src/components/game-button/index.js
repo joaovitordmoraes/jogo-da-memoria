b85957ced8195b44dbf40d66738dc4c6480fe1e3
João Vitor <joaovitordmoraes@gmail.com>
2019-09-11 00:16:13 -0300

    Desafio 02 - Aula 33

--- src/components/game-button/index.js
+++ src/components/game-button/index.js
@@ -1,7 +1,35 @@
 const gameButton = (function() {
     const module = {};
 
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = `
+            .game-button {
+                position: fixed;
+                bottom: 50%;
+                left: 50%;
+                transform: translate(-50%, 0);
+                z-index: 2;
+                border: 4px solid #575555;
+                height: 120px;
+                width: 120px;
+                border-radius: 100%;
+                box-shadow: 0 0 40px 0 #000;
+                background-color: #3a4042;
+                color: #fff;
+                font-family: 'Comfortaa', sans-serif;
+                font-weight: bold;
+                cursor: pointer;
+            }
+        `;
+
+        $head.insertBefore($style, null);
+    };
+
     module.create = () => {
+        module._style();
         return `
             <button class="game-button">
                 Iniciar Jogo
