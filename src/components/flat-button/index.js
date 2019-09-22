913599ee83c9844e3551813bf575d1fa51f6ca4d
João Vitor <joaovitordmoraes@gmail.com>
2019-09-22 20:48:15 -0300

    Aula 38

---
+++ src/components/flat-button/index.js
@@ -0,0 +1,35 @@
+const flatButton = (function() {
+    const module = {};
+
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = `
+            .flat-button {
+                background-color: #eae6da;
+                font-size: 24px;
+                font-weight: bold;
+                color: #fffcee;
+                text-transform: uppercase;
+                border: 0;
+                width: 50%;
+                height: 176px;
+            }
+        `;
+
+        $head.insertBefore($style, null);
+    };
+
+    module.render = () => {
+        module._style();
+
+        return `
+            <button class="flat-button">Login</button>
+        `;
+    };
+
+    return {
+        render: module.render
+    };
+})();
