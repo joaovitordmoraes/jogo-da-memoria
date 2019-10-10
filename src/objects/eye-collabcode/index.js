6256bfbd2e2b0341a2ad258b6b9d9df037951a9e
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-10 16:12:22 -0300

    Criado o object do eye-collabcode

---
+++ src/objects/eye-collabcode/index.js
@@ -0,0 +1,22 @@
+const eyeCollabcode = (function() {
+    const module = {};
+
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = ``;
+
+        $head.insertBefore($style, null);
+    };
+
+    module.render = () => {
+        module._style();
+
+        return ``;
+    };
+
+    return {
+        render: module.render
+    };
+})();
