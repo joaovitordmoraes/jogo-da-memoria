4e481979ce2da6c0c867d4664ad3a14e39bd8c47
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-10 13:02:40 -0300

    Ajuste link forget-password

--- src/objects/forget-password/index.js
+++ src/objects/forget-password/index.js
@@ -9,21 +9,21 @@ const forgetPassword = (function() {
             .forget-password {
                 font-size: 14px;
                 color: rgba(58,64,66,.5);
-                text-align: right;
                 text-decoration: none;
                 width: 100%;
-                display: inline-block;
+                display: block;
             }
 
             .input-collabcode + .forget-password {
-                margin: 40px 0 110px 0;
+                margin: 40px 0 60px 0;
+                text-align: right;
             }
         `;
 
         $head.insertBefore($style, null);
     };
 
-    module.render = ({ content, href }) => {
+    module.render = ({ content, href = "#" }) => {
         module._style();
 
         return `<a href="${href}" class="forget-password">${content}</a>`;
